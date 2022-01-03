0ce162bc6f64d688c45d59cf708976294c7fb3d8
片岡潤隆 <>
2022-01-03 14:52:30 +0900

    ♻️ リファクタリング

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import { Main } from './../components/index';
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Main />
