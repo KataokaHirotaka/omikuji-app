ac8b4f6ea3bf055631974674516d72027af8f8b9
片岡潤隆 <>
2022-01-06 04:03:04 +0900

    おみくじ結果のローディング機能を追加

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Button } from './index';
 import styles from './styles/Result.module.scss';
 
@@ -9,16 +9,22 @@ type Props = {
 }
 
 export const Result = ({ randomNum, setClickFlag }: Props) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const handleClick = useCallback(() => {
     setClickFlag(false);
+    setIsLoading(false);
   }, []);
 
+  useEffect(() => {
+    setIsLoading(true);
+  }, [randomNum]);
+  
   return (
     <div className={styles.result_content_wrapper} onClick={handleClick}>
       <div className={styles.close_button_wrapper} onClick={handleClick}>
         <Button text='' id="close"/>
       </div>
-      <div className={styles.result_wrapper}>
+      <div className={styles.result_wrapper} style={{display: isLoading ? 'block' : 'none'}}>
         <Image
           src={randomNum != undefined ? `/img/${randomNum}.png` : '/img/1.png'}
           alt="おみくじの結果"
